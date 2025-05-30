4ab79d1ac191196b4aeee4ebab358123bb0a4acb
Raum <198965701+simplespace-cat@users.noreply.github.com>
2025-05-30 16:26:08 +0100

    Windows

--- app/src/main-process/main.ts
+++ app/src/main-process/main.ts
@@ -118,7 +118,7 @@ if (__DARWIN__) {
 // On Windows, in order to get notifications properly working for dev builds,
 // we'll want to set the right App User Model ID from production builds.
 if (__WIN32__ && __DEV__) {
-  app.setAppUserModelId('com.squirrel.GitHubDesktop.GitHubDesktop')
+  app.setAppUserModelId('com.squirrel.GitCub.GitCub')
 }
 
 app.on('window-all-closed', () => {
--- app/static/win32/github.sh
+++ app/static/win32/github.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CONTENTS="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")")"
-ELECTRON="$CONTENTS/GitHubDesktop.exe"
+ELECTRON="$CONTENTS/GitCub.exe"
 
 if grep -q Microsoft /proc/version; then
 	if [ -x /bin/wslpath ]; then
